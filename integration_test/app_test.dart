b601d35ebaae3da24c4a6803faa16d77add4549b
amrutha <amruthar@testvagrant.com>
2024-02-14 11:36:39 +0530

    initial commit

---
+++ integration_test/app_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main(){
+  group('E2E test', () { 
+    testWidgets('test Search bar', (tester) => null)
+  });
+}
